e1ea40869da5ac7bb4816e4b4b750a6b09eae772
Ravan <eliyevrevan60@gmail.com>
2024-11-02 12:21:14 +0400

    a

---
+++ src/Alphabet.java
@@ -0,0 +1,14 @@
+public class Alphabet {
+
+    public static void main(String[] args){
+
+        System.out.println("Uppercase Alphabet: ");
+        for (char c = 'A'; c<='Z'; c++){
+            System.out.print(c);
+        }
+        System.out.println("\nLowercase Alphabet: ");
+        for(char c = 'a'; c <= 'z'; c++){
+            System.out.print(c);
+        }
+    }
+}
