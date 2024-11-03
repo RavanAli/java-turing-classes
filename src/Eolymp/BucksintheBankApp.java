d7d5394cd2f667266f772a92f18b0d09893dbfc8
Ravan <eliyevrevan60@gmail.com>
2024-11-03 23:31:23 +0400

    a

---
+++ src/Eolymp/BucksintheBankApp.java
@@ -0,0 +1,22 @@
+package Eolymp;
+
+import java.util.Scanner;
+
+public class BucksintheBankApp {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        long S = scanner.nextLong();
+        long total = 1;
+        long given = 1;
+        int birthday = 1;
+
+        while(total < S){
+            birthday++;
+            given = 2 * given + birthday;
+            total =+given;
+        }
+        System.out.println(S + " " + birthday);
+
+    }
+}
