4bfa9e071eb4946b6b571480eee6215397459152
Ravan <eliyevrevan60@gmail.com>
2024-11-03 22:13:44 +0400

    a

---
+++ src/Eolymp108.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Eolymp108 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int num3 = scanner.nextInt();
+
+        if((num1 < num2 && num1 > num3) ||(num1 > num2 && num1 < num3)){
+            System.out.println("Median is: " + num1);
+        } else if ((num2 < num1 && num2 > num3) || (num2 > num1 && num2 < num3)) {
+            System.out.println("Median is: " +num2);
+        }else {
+            System.out.println("Median is: " +num3);
+        }
+
+    }
+}
