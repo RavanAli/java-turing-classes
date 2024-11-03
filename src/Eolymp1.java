744c6bc63ccb108bbda593080d9f5eeca8ab1af9
Ravan <eliyevrevan60@gmail.com>
2024-11-03 21:46:09 +0400

    a

--- src/Eolymp1.java
+++ src/Eolymp1.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
+
+import static java.lang.System.in;
 
 public class Eolymp1 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println("Enter a number: ");
+        System.out.println(number/10 +" "+ number%10 );
+    }
 }
