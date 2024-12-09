6760b308461918afbce9f42613affb90ca053aad
Ravan <eliyevrevan60@gmail.com>
2024-12-10 00:47:41 +0400

    Bank

--- src/Bank/CurrentAccount.java
+++ src/Bank/CurrentAccount.java
@@ -1,4 +1,31 @@
 package Bank;
 
-public class CurrentAccount {
+public class CurrentAccount extends BankAccount{
+    private double overdraftLimit;
+
+    public CurrentAccount(String accountHolder, double balance, int accountNumber, double overdraftLimit){
+        super(accountHolder,balance,accountNumber);
+        this.overdraftLimit = overdraftLimit;
+    }
+    public  double getOverdraftLimit(){
+        return overdraftLimit;
+    }
+    public void setOverdraftLimit(){
+        this.overdraftLimit = overdraftLimit;
+    }
+
+    @Override
+    public void displayInfo(){
+        super.displayInfo();
+        System.out.println("Overdraft Limit: " + overdraftLimit);
+    }
+    @Override
+    public void withdraw(double amount){
+        if (getBalance() + overdraftLimit >= amount){
+            setBalance(getBalance() - amount);
+            System.out.println("Withdrawn: " + amount);
+        }else {
+            System.out.println("Invalid");
+        }
+    }
 }
--- src/Bank/SavingsAccount.java
+++ src/Bank/SavingsAccount.java
@@ -1,4 +1,45 @@
 package Bank;
 
-public class SavingsAccount {
+public class SavingsAccount extends BankAccount{
+    private double monthlyInterestRate;
+    private int withdrawalLimit = 3;
+    private int withdrawalMade = 0;
+
+    public SavingsAccount(String accountHolder, double balance, int accountNumber, double monthlyInterestRate){
+        super(accountHolder,balance,accountNumber);
+        this.monthlyInterestRate= monthlyInterestRate;
+    }
+
+    public double getMonthlyInterestRate() {
+        return monthlyInterestRate;
+    }
+    public void setMonthlyInterestRate(){
+        this.monthlyInterestRate = monthlyInterestRate;
+    }
+    public int getWithdrawalLimit(){
+        return withdrawalLimit;
+    }
+    public int getWithdrawalMade(){
+        return withdrawalMade;
+    }
+    @Override
+    public void displayInfo(){
+        super.displayInfo();
+        System.out.println("Monthly Interest Rate: " + monthlyInterestRate + "Remaining withdraw: " + (withdrawalLimit - withdrawalMade));
+    }
+
+    @Override
+    public void withdraw(double amount){
+        if (withdrawalMade < withdrawalLimit){
+            super.withdraw(amount);
+            withdrawalMade++;
+        }else {
+            System.out.println("Reached withdraw limit");
+        }
+    }
+    public void applyInterest(){
+        double interest = getBalance() * (monthlyInterestRate / 100);
+        setBalance(getBalance() + interest);
+        System.out.println("Interest applied: " + interest);
+    }
 }
